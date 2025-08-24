a337fae80e1d601e492cb36bd0ff0b3960235266
Waariha Asim <163326243+Wariha-Asim@users.noreply.github.com>
2025-08-24 05:30:30 +0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -249,4 +249,12 @@ document.addEventListener("DOMContentLoaded", () => {
       this.classList.add("active");
     });
   });
+  const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+if (menuToggle && mobileMenu) {
+  menuToggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+}
 });
